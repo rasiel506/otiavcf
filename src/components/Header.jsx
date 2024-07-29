4ac1c44514f841a7e7a6b23fa97f55730fcf119c
rasiel506 <rimgiorgi@gmail.com>
2024-07-29 17:43:52 +0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   return (
     <div className="h-16 px-3 bg-darkBlue1 text-slate-300 w-full flex items-center border-b-2 border-lightGray">
       <div className="flex-grow font-semibold">
-        <Link to="/">Video and Voice</Link>
+        <Link to="/">Connect</Link>
       </div>
       <div>
         {user ? (

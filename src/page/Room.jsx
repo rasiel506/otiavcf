f609ba32269a388a2bc1543d6d5221389f6ef3e3
rasiel506 <rimgiorgi@gmail.com>
2024-07-29 21:34:29 +0400

    Update Room.jsx

--- src/page/Room.jsx
+++ src/page/Room.jsx
@@ -155,7 +155,7 @@ const Room = () => {
     // Send roomID to backend API
     const sendRoomIDToAPI = async () => {
       try {
-        const response = await fetch("https://your-backend-api-url.com/room", {
+        const response = await fetch("https://otia.ai/6/updateRoom.php", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",

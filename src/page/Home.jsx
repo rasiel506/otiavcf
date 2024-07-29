66e63c50b49f2dd440137b30c29c4743ed476066
rasiel506 <rimgiorgi@gmail.com>
2024-07-29 18:36:04 +0400

    Update Home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,38 +1,34 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import HomeCard from "../components/HomeCard";
-
 import { v4 as uuid } from "uuid";
 
 // icons
 import { MdVideoCall as NewCallIcon } from "react-icons/md";
-import { MdAddBox as JoinCallIcon } from "react-icons/md";
-import { BsCalendarDate as CalenderIcon } from "react-icons/bs";
-import { MdScreenShare as ScreenShareIcon } from "react-icons/md";
-import { Link } from "react-router-dom";
-
-const roomId = uuid();
 
 const Home = () => {
- 
+  const navigate = useNavigate();
+  const roomId = uuid();
+
+  useEffect(() => {
+    navigate(`/room/${roomId}`);
+  }, [navigate, roomId]);
 
   return (
     <div className="bg-darkBlue1 min-h-screen text-slate-400">
       <div className="flex h-full md:gap-2 flex-col md:flex-row">
         <div className="p-3 w-auto h-auto items-center">
           <div className="flex gap-2 md:gap-6 mb-3 md:mb-6">
-            <Link to={`/room/${roomId}`} className="block w-full">
-              <HomeCard
-                title="New Meeting"
-                desc="Create a new meeting"
-                icon={<NewCallIcon />}
-                iconBgColor="lightYellows"
-                bgColor="bg-yellow"
-                route={`/room/`}
-              />
-            </Link>
+            <HomeCard
+              title="New Meeting"
+              desc="Create a new meeting"
+              icon={<NewCallIcon />}
+              iconBgColor="lightYellows"
+              bgColor="bg-yellow"
+              route={`/room/`}
+            />
           </div>
-          <div>
-          </div>
+          <div></div>
         </div>
       </div>
     </div>
